57f947e77e76c41a9f9a7a5d9f5a4fc42aebcca0
austindflatt <austindflatt@gmail.com>
2021-09-11 23:00:54 -0400

    created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Products from './components/Products/Products'
+
+import { Navbar, Products } from './components';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   )
