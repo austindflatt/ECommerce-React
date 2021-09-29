8c2b727953577784948e964e0efc13d0eb458226
austindflatt <austindflatt@gmail.com>
2021-09-29 19:23:42 -0400

    added return operator for empty cart and shopping cart with items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,10 +3,20 @@ import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 const Cart = () => {
     const isEmpty = true;
+    const EmptyCart = () => (
+        <Typography variant="subtitle1">There are no items in your shopping cart</Typography>
+
+    );
+
+    const FilledCart = () => (
+        <>
+        </>
+    );
     return (
         <Container>
             <div className={classes.toolbar} />
             <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+            { isEmpty ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
 }
